47a9c657cbc113929bbd278e4ed453036da3bb9e
Minh Son Tran <tranminhson1989@gmail.com>
2020-12-09 14:38:10 -0500

    Add Footstep Sound

---
+++ Assets/_Scripts/CharacterFootstepAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFootstepAudio : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip[] footstepSounds;
+
+    [SerializeField]
+    AudioSource footstepSource;
+
+    [Tooltip("Variance with which to pitch footsteps up/down both ways from 1")]
+    [SerializeField]
+    float pitchVariance = 2.0f;
+
+    public void PlayFootstep()
+    {
+        footstepSource.clip = footstepSounds[0];
+        // footstepSource.clip = footstepSounds[Random.Range(0, footstepSounds.Length)];
+
+        footstepSource.pitch = Random.Range(1.0f - pitchVariance, 1.0f + pitchVariance);
+
+        footstepSource.Play();
+    }
+}
